81c48fdbdb7646f973cb90cc115f62da14a55c33
shefali singh <shefalisingh@microsoft.com>
2022-06-24 00:08:22 +0530

    removing sudo

--- ci/scripts/install_azurite.sh
+++ ci/scripts/install_azurite.sh
@@ -3,9 +3,9 @@
 # Error out on first failure
 set -e
 
-sudo apt-get -y install nodejs
+apt-get -y install nodejs
 echo "node version = `node --version`"
-sudo npm install -g azurite
+npm install -g azurite
 AZURITE_DIR=${1}/azurite
 mkdir $AZURITE_DIR
 which azurite
