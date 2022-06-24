8d0d41315dedaacf389c9195968d3b2027ba4724
shefali singh <shefalisingh@microsoft.com>
2022-06-24 11:17:09 +0530

    removed script

--- ci/scripts/install_azurite.sh
+++ ci/scripts/install_azurite.sh
@@ -3,13 +3,13 @@
 # Error out on first failure
 set -e
 
-apt-get -y install nodejs
-echo "node version = `node --version`"
-npm install -g azurite
-AZURITE_DIR=${1}/azurite
-mkdir $AZURITE_DIR
-which azurite
-echo "azurite version = `azurite --version`"
+# apt-get -y install nodejs
+# echo "node version = `node --version`"
+# npm install -g azurite
+# AZURITE_DIR=${1}/azurite
+# mkdir $AZURITE_DIR
+# which azurite
+# echo "azurite version = `azurite --version`"
 
-# Start azurite
-azurite --silent --location $AZURITE_DIR --debug $AZURITE_DIR/debug.log
+# # Start azurite
+# azurite --silent --location $AZURITE_DIR --debug $AZURITE_DIR/debug.log
