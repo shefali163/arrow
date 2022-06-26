ee9a538edc31e8cb4dfa40bdff0f1a2398d5f78c
shefali singh <shefalisingh@microsoft.com>
2022-06-26 13:43:08 +0530

    added 1

--- ci/scripts/install_azurite.sh
+++ ci/scripts/install_azurite.sh
@@ -23,10 +23,10 @@ set -e
 brew install node
 echo "node version = `node --version`"
 npm install -g azurite
-# AZURITE_DIR=${0}/azurite
-# mkdir $AZURITE_DIR
 which azurite
 echo "azurite version = `azurite --version`"
+AZURITE_DIR=${1}/azurite
+mkdir $AZURITE_DIR
 
-# # Start azurite
-# azurite --silent --location $AZURITE_DIR --debug $AZURITE_DIR/debug.log
+# Start azurite
+azurite --silent --location $AZURITE_DIR --debug $AZURITE_DIR/debug.log
